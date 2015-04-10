b277047e806f00f1a91424a9770a675b56bf4ae8
Morgan Croney <m@morgancroney.com>
2015-04-10 12:30:06 -0400

    Simplify requires.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // this will set up things
 'use strict';
 
-var express = require('express'),
-  routes = require('./services/routes'),
-  composer = require('./services/composer'),
-  app = express();
+var app = require('express')(),
+  routes = require('./services/routes');
 
 module.exports = function () {
   return routes(app);
