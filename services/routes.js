c5526074ea53d6300ca97e08d789429522469822
Dane Stuckel <dane.stuckel@gmail.com>
2015-05-01 19:01:43 -0400

    new page

--- services/routes.js
+++ services/routes.js
@@ -352,6 +352,64 @@ function routeByExtension(req, res) {
 }
 
 /**
+ * First draft
+ * @param req
+ * @param res
+ */
+function createPage(req, res) {
+  function getUniqueId() {
+    return flake.next().toString('base64');
+  }
+
+  var ops = [],
+    Flake = require('flake-idgen'),
+    flake = new Flake(),
+    body = req.body,
+    layoutReference = body && body.layout,
+    pageData = body && _.omit(body, 'layout'),
+    pageReference = '/pages/' + getUniqueId();
+
+  pageData = _.reduce(pageData, function (obj, value, key) {
+    //create new copy of component from defaults
+    var componentName = references.getComponentName(value);
+
+    obj[key] = references.getComponentData('/components/' + componentName).then(function (componentData) {
+      var componentInstance = '/components/' + componentName + '/instances/' + getUniqueId();
+      ops.push({
+        type: 'put',
+        key: componentInstance,
+        value: componentData
+      });
+      return componentInstance;
+    });
+
+    return obj;
+  }, {
+    layout: layoutReference
+  });
+
+  bluebird.props(pageData)
+    .then(function (value) {
+
+      ops.push({
+        type: 'put',
+        key: pageReference,
+        value: value
+      });
+
+      return db.batch(ops).then(function () {
+        //if successful, return new page object, but include the (optional) self reference to the new page.
+        value._ref = pageReference;
+        return value;
+      });
+    }).then(function (result) {
+      res.send(result);
+    }).catch(function (err) {
+      log.error('Failed to create new page' + err.stack);
+    });
+}
+
+/**
  * Add component routes to this router.
  * @param router
  */
@@ -375,6 +433,7 @@ function addComponentRoutes(router) {
   router.get('/pages', listAllWithPrefix);
   router.get('/pages/:name', getRouteFromDB);
   router.put('/pages/:name', putRouteFromDB);
+  router.post('/pages', createPage);
 
   router.get('/uris', listAllWithPrefix);
   router.get('/uris/:name', getRouteFromDB);
