4aa8616cf9df3a7ddfbfbb013fd242be3cfe127f
Dane Stuckel <dane.stuckel@gmail.com>
2015-10-20 17:31:42 -0400

    add more tests suggested by Morgan

--- test/api/pages/delete.js
+++ test/api/pages/delete.js
@@ -52,6 +52,19 @@ describe(endpointName, function () {
       acceptsHtml(path, {name: 'missing'}, 406, '406 text/html not acceptable');
     });
 
+    describe('/pages/:name.html', function () {
+      var path = this.title;
+
+      acceptsJson(path, {name: 'valid'}, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+      acceptsJson(path, {name: 'missing'}, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+
+      acceptsJsonBody(path, {name: 'valid'}, pageData, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+      acceptsJsonBody(path, {name: 'missing'}, pageData, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+
+      acceptsHtml(path, {name: 'valid'}, 405, '405 Method DELETE not allowed');
+      acceptsHtml(path, {name: 'missing'}, 405, '405 Method DELETE not allowed');
+    });
+
     describe('/pages/:name@:version', function () {
       var path = this.title;
 
@@ -63,6 +76,19 @@ describe(endpointName, function () {
 
       acceptsHtml(path, {name: 'valid', version: 'valid'}, 406, '406 text/html not acceptable');
       acceptsHtml(path, {name: 'valid', version: 'missing'}, 406, '406 text/html not acceptable');
+    });
+
+    describe('/pages/:name@:version.html', function () {
+      var path = this.title;
+
+      acceptsJson(path, {name: 'valid', version: 'valid'}, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+      acceptsJson(path, {name: 'valid', version: 'missing'}, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+
+      acceptsJsonBody(path, {name: 'valid', version: 'valid'}, pageData, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+      acceptsJsonBody(path, {name: 'valid', version: 'missing'}, pageData, 405, { message: 'Method DELETE not allowed', code: 405, allow: ['get'] });
+
+      acceptsHtml(path, {name: 'valid', version: 'valid'}, 405, '405 Method DELETE not allowed');
+      acceptsHtml(path, {name: 'valid', version: 'missing'}, 405, '405 Method DELETE not allowed');
     });
   });
 });
