ddc46804bd77f15e9be109d82ad3832ff84b0d7e
Jon Winton <winton.jon@gmail.com>
2016-12-06 15:25:11 -0500

    Adding elastic pages data

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ module.exports.pages = require('./lib/services/pages');
 module.exports.sites = require('./lib/services/sites');
 module.exports.references = require('./lib/services/references');
 module.exports.log = require('./lib/services/log');
-module.exports.setPageName = require('./lib/services/page-list').setPageName;
+module.exports.pageTitleService = require('./lib/services/page-list').pageTitleService;
+module.exports.pageAuthorsService = require('./lib/services/page-list').pageAuthorsService;
