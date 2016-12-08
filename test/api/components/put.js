b183179e5407ad891363952c88bc57c55af674c8
Nelson Pecora <nelson@yoshokatana.com>
2016-12-08 12:39:27 -0500

    compose json endpoints (#345)
    * add composing to component and page endpoints
    
    * updated tests
    
    * updated docs
    
    * updated coverage
    
    * update readme

--- test/api/components/put.js
+++ test/api/components/put.js
@@ -185,7 +185,7 @@ describe(endpointName, function () {
       acceptsJsonBody(path, {name: 'invalid', id: 'valid'}, {}, 404, { code: 404, message: 'Not Found' });
       acceptsJsonBody(path, {name: 'valid', id: 'valid'}, data, 200, data);
       acceptsJsonBody(path, {name: 'missing', id: 'missing'}, data, 200, data);
-      acceptsJsonBody(path, {name: 'valid'}, cascadingData(), 200, cascadingReturnData());
+      acceptsJsonBody(path, {name: 'valid'}, cascadingData(), 200, cascadingData());
 
       acceptsHtml(path, {name: 'invalid', id: 'valid'}, 404);
       acceptsHtml(path, {name: 'valid', id: 'valid'}, 406);
