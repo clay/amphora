b530cba3d105221c080a069301d7fcb355907c46
Jon Winton <winton.jon@gmail.com>
2018-06-13 15:54:52 -0400

    first pass

--- test/index.js
+++ test/index.js
@@ -13,13 +13,9 @@ chai.config.truncateThreshold = 0;
 // make sure the index file can be loaded at least
 require('..');
 
-_.each(apiTests, function (test) {
-  require(test);
-});
+_.each(apiTests, test => require(test));
 
-_.each(tests, function (test) {
-  require(test);
-});
+_.each(tests, test => require(test));
 
 after(function () {
   require('./fixtures/enforce-performance')(this);
